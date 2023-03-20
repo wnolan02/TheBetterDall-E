d4c7442d2dce144441d620758eca9cda334c42b9
Waylon <maxperform24@gmail.com>
2023-03-20 11:01:37 -0500

    Api add
    Changing localhost to live backend API (render)

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -20,7 +20,7 @@ const generateImage = async () => {
   if(form.prompt) {
     try {
       setGeneratingImg(true)
-      const response = await fetch('http://localhost:8080/api/v1/dalle', {
+      const response = await fetch('https://thebetterdall-e.onrender.com/api/v1/dalle', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -49,7 +49,7 @@ const handleSubmit = async (e) => {
     setLoading(true)
 
     try {
-      const response = await fetch('http://localhost:8080/api/v1/post', {
+      const response = await fetch('https://thebetterdall-e.onrender.com/api/v1/post', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
